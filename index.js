a23165e91cbd96027cf5f71e5cc52872ad810311
Gurnoor25 <66201682+Gurnoor25@users.noreply.github.com>
2022-10-10 18:04:37 +0530

    First Files for Calculator
    Used Eval but will do it in stack implementation.

--- index.js
+++ index.js
@@ -7,63 +7,133 @@ for (let i=0;i<numNodes.length;i++){
 for (let i=0;i<opNodes.length;i++){
     opNodes[i].addEventListener('click',display);
 }
-
-let checkNum = ['0','1','2','3','4','5','6','7','8','9','.'];
-let checkOp  = ['+','-','*','%','/'];
-
-
 let completeValue = "";
-function display(){
-    screenValue=this.firstElementChild.innerHTML;
+function display(){                                                                                           //TO PASS THE INPUT VALUE FOR COMPUTATION
+    screenValue=this.innerHTML;
+    console.log(screenValue);
     if (screenValue=="="){
-        myfunc(completeValue);
-        document.getElementById("screen").value= eval(completeValue);
-        completeValue=document.getElementById("screen").value;
+        // myfunc(completeValue);
+        document.getElementById("output-value").value= eval(completeValue);                 //USED EVAL FOR NOW AS THERE WERE FEW BUGS IN CODE , WiLL UPDATE BY TOMORROW
+        // console.log(completeValue);
+        // document.getElementById("output-value").value = myfunc(completeValue);
+        completeValue=document.getElementById("output-value").value;
         return;
     }
-    else if (screenValue == "=&gt;"){
+    else if (screenValue == "CE"){
         completeValue = completeValue.substring(0,completeValue.length-1);
-        document.getElementById("screen").value = completeValue;
+        document.getElementById("output-value").value = completeValue;
     }
-    else if(screenValue=="AC") {
-        document.getElementById("screen").value = "";
+    else if(screenValue=="C") {
+        document.getElementById("output-value").value = "";
         completeValue="";
-
+ 
     }
     else{
-           document.getElementById("screen").value += screenValue;
+           document.getElementById("output-value").value += screenValue;
            completeValue+=screenValue;
     }
 }
-let numStack = [];
-let opStack =[];
-function myfunc(cv){
-    let numStart = 0;
-    let numEnd = 0;
-    for(let i=0;i<cv.length;i++){
-        if (checkNum.includes(cv[i]))
-           {
+// let checkNum = ['0','1','2','3','4','5','6','7','8','9','.'];
+// let checkOp  = ['+','-','*','%','/'];                                                      //STACK IMPLEMENTATION WITH CORRECT PRECEDENCE
+// let numStack = [];
+// let opStack =[];
+// let NeedOperation = false;
+// let precendenceOrder =[];
+// let result = 0;
+// function myfunc(cv){                                                                          //TO CHECK FOR CALCULATION
+//     let numStart = 0;
+//     let numEnd = 0;
+//     for(let i=0;i<cv.length;i++){
+//         if (checkNum.includes(cv[i]))
+//            {
             
-            numEnd = i;
-            if (i==cv.length-1){
-                numStack.push(cv.substring(numStart,numEnd+1));
-            }
-            }
-        else {
-           numStack.push(cv.substring(numStart,numEnd+1));
-           opStack.push(i);
-           checkPrecedence(cv[i]);
-           if(i+1<cv.length)
-               {
-                    numStart = i+1;
-                } 
-             }
-        console.log(cv);
-    }
-}
+//             numEnd = i;
+//             if (i==cv.length-1){
+//                 numStack.push(cv.substring(numStart,numEnd+1));
+//                 while(opStack.length != 0){
+//                     result = finalCalculation();
+//                     numStack.push(result);
+//                 }
+//                 document.getElementById("output-value").value = result;
+//             }
+//             }
+//         else {
+//            numStack.push(cv.substring(numStart,numEnd+1));
+//            NeedOperation = checkPrecedence(cv[i]);
 
-function checkPrecedence(symbol){
+//            while(NeedOperation){
+//                 result  = finalCalculation();
+//                 numStack.push(result);
+//                 NeedOperation = checkPrecedence(cv[i]);
+                
+
+//             }
+             
+//                 opStack.push(cv[i]);
+//            if(i+1<cv.length)
+//                {
+//                     numStart = i+1;
+//                 } 
+//              }
+//         console.log(cv);
+//     }
+// }
+
+// function checkPrecedence (op){                                          // CHECK PRECEDENCE 
+//     if (precendenceOrder.length==0){
+//         switch(op){                                                                             
+//             case "+": precendenceOrder.push(1);
+//                 break;
+//             case "-": precendenceOrder.push(1);
+//                 break;
+//             case "*":  precendenceOrder.push(2);
+//                 break;
+//             case "/":  precendenceOrder.push(2);
+//                 break;            
+//         }
+//     }
+
+//     else {
+//         let CurrentPrecedence = givePrecedence(op);
+//         if (CurrentPrecedence < precendenceOrder[precendenceOrder.length-1]){
+//             return true;
+//         }
+//         else {
+//             precendenceOrder.push(CurrentPrecedence);
+//         }
+
+//     }
+
+// }
+
+// function givePrecedence (op){                               //GIVES THE PRECEDENCE VALUE OF CURRENT OPERATOR
+
+//     switch(op){
+//         case "+": return 1;
+//             break;
+//         case "-": return 1;
+//             break;
+//         case "*":  return 2;
+//             break;
+//         case "/":  return 2;
+//             break;            
+//     }
+// }
 
 
+// function finalCalculation(){                                   // FINAL CALCULATION USING 2 Operands and 1 Operator
+//     let num2 = parseFloat(numStack.pop());
+//     let num1 = parseFloat(numStack.pop());
+//     let op = opStack.pop();
+//     switch(op){
+//         case "+": return num1 + num2;
+//             break;
+//         case "-": return num1 - num2;
+//             break;
+//         case "*":  return num1 * num2;
+//             break;
+//         case "/":  return num1 / num2;
+//             break;            
+//     }
 
-}
+// }
